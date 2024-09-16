038769fac5a37164e9196afc3ff4ca262969e3eb
Lee Robinson <me@leerob.io>
2024-09-16 14:32:05 -0500

    Add playlist for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { getPlaylist } from '@/lib/db/queries';
 import { TrackTable } from './p/[id]/track-table';
 
-export default function AllTracksPage() {
+export default async function AllTracksPage() {
+  const playlist = await getPlaylist('1');
+
   return (
     <div className="flex-1 flex flex-col overflow-hidden bg-[#0A0A0A]">
       <ScrollArea className="flex-1">
         <div className="min-w-max">
-          <TrackTable />
+          <TrackTable playlist={playlist} />
         </div>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
