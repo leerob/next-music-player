0ec1dc11e6d0f822bd9cbb6fabebf81cc23ed3bb
Lee Robinson <me@leerob.io>
2024-10-02 17:36:56 -0500

    dynamicIO

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { TrackTable } from './p/[id]/track-table';
 import { getAllSongs, searchSongs } from '@/lib/db/queries';
+import { Suspense } from 'react';
 
-export default async function AllTracksPage({
+async function Tracks({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>;
 }) {
   const query = (await searchParams).q;
   const songs = query ? await searchSongs(query) : await getAllSongs();
+  // @ts-ignore
+  return <TrackTable query={query} playlist={{ songs }} />;
+}
 
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
   return (
     <div className="flex-1 flex flex-col overflow-hidden bg-[#0A0A0A] pb-[69px] pt-2">
       <ScrollArea className="flex-1">
         <div className="min-w-max">
-          {/* @ts-ignore */}
-          <TrackTable query={query} playlist={{ songs }} />
+          <Suspense fallback={<div className="w-full" />}>
+            <Tracks searchParams={searchParams} />
+          </Suspense>
         </div>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>
--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     ppr: true,
+    dynamicIO: true,
     reactCompiler: true,
     serverActions: {
       bodySizeLimit: '5mb',
